95101446738b5f7d3eb490cc1ae5b781d30201cc
Renato <renato87rj@gmail.com>
2020-09-01 00:51:19 -0300

    adicionando imagems e textos aos cards de resultados

--- src/components/results-card/index.js
+++ src/components/results-card/index.js
@@ -33,11 +33,15 @@ const useStyles = makeStyles((theme) => ({
     transform: 'rotate(180deg)',
   },
   avatar: {
-    backgroundColor: '#fff',
-    color: '#333'
+    backgroundColor: '#333',
+    color: '#fff'
   },
   title: {
     fontSize: 22
+  },
+  textBold: {
+    fontWeight: "bold",
+    fontSize: 14
   }
 }));
 
@@ -77,31 +81,29 @@ export default function ResultsCard(props) {
         >
           <ExpandMoreIcon />
         </IconButton>
+        Ver descrição
       </CardActions>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography paragraph>Method:</Typography>
+          <Typography paragraph>Comportamentos: <span className={classes.textBold}>{props.comportamento}</span></Typography>
           <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-            minutes.
+            {props.textoComportamento}
           </Typography>
+          <Typography paragraph>Ponto Forte: <span className={classes.textBold}>{props.pontoForte}</span></Typography>
           <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-            and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-            pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+            {props.textoPontoForte}
           </Typography>
+          <Typography paragraph>Pontos de Melhoria: <span className={classes.textBold}>{props.pontosMelhoria}</span></Typography>
           <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-            without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-            medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-            again without stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don’t open.)
+            {props.textoPontosMelhoria}
           </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
+          <Typography paragraph>Motivações:</Typography>
+          <Typography paragraph>
+            {props.textoMotivacoes}
+          </Typography>
+          <Typography paragraph>Valores que movem:</Typography>
+          <Typography paragraph>
+            {props.textoValores}
           </Typography>
         </CardContent>
       </Collapse>
--- src/pages/results/index.js
+++ src/pages/results/index.js
@@ -1,19 +1,58 @@
 import React from 'react';
 
-import ResultsCard from '../../components/results-card';
-import { Grid, makeStyles, Container } from '@material-ui/core';
+import { Grid, makeStyles, Container, Paper, Typography } from '@material-ui/core';
 
+import ResultsCard from '../../components/results-card';
 import Tubarao from '../../assets/images/tubarao.jpg';
 import Lobo from '../../assets/images/lobo.jpg';
 import Aguia from '../../assets/images/aguia.jpg';
 import Gato from '../../assets/images/gato.jpg';
+import Cerebro from '../../assets/images/cerebro-direito.jpg'
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
     root: {
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center'
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+    },
+    cerebroDireito: {
+        position: 'relative',
+        zIndex: 1,
+        '&::before': {
+            content: '""',
+            position: 'absolute',
+            zIndex: '-1',
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            background: 'url('+Cerebro+') center center',
+            backgroundSize: 'cover',
+            opacity: '0.1',
+        }
+    },
+    
+    cerebroEsquerdo: {
+        position: 'relative',
+        zIndex: 1,
+        '&::before': {
+            content: '""',
+            position: 'absolute',
+            zIndex: '-1',
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            background: 'url('+Cerebro+') center center',
+            transform: 'scaleX(-1)',
+            backgroundSize: 'cover',
+            opacity: '0.1',
+        }
     },
   }));
 
@@ -23,12 +62,35 @@ export default function Results() {
     return (
         <div className={classes.root}>
             <Container maxWidth="lg">
+            <Typography variant="h2" className={classes.root}>Resultados</Typography>
+                <Grid container spacing={4}>
+                    <Grid item xs={6} sm={6}>
+                        <Paper className={[classes.paper, classes.cerebroEsquerdo]}>
+                            <Typography variant="button" paragraph>Lado Esquerdo do Cérebro</Typography>
+                            <Typography variant="subtitle2" paragraph>Emocional:</Typography>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={6} sm={6}>
+                        <Paper className={[classes.paper, classes.cerebroDireito]}>
+                            <Typography variant="button" paragraph>Lado Direito do Cérebro</Typography>
+                            <Typography variant="subtitle2" paragraph>Racional:</Typography>
+                        </Paper>
+                    </Grid>
+                </Grid>
                 <Grid container spacing={2}>
                     <Grid item xs={6} sm={3}>
                         <ResultsCard 
                             title="Tubarão" 
                             image={Tubarao}
                             imageTitle="tubarão"
+                            comportamento="Fazer rápido"
+                            textoComportamento="Senso de urgência. Ação iniciativa. Impulsivo, prático. Vencer desafios. Aqui e agora. Auto suficiente. Não gosta de delegar poder"
+                            pontoForte="Ação"
+                            textoPontoForte="Fazer que ocorra. Parar com burocracia. Motivação"
+                            pontosMelhoria="Ação"
+                            textoPontosMelhoria="Socialmente um desastre. Faz do modo mais fácil. Relacionamento complicado"
+                            textoMotivacoes="Liberdade para agir individualmente. Controle das próprias atividades. Resolver os problemas do seu jeito. Competição individual. Variedade de atividades. Não ter que repetirtarefas"
+                            textoValores="Resultados"
                         />
                     </Grid>
                     <Grid item xs={6} sm={3}>
@@ -36,6 +98,14 @@ export default function Results() {
                             title="Lobo"
                             image={Lobo}
                             imageTitle="lobo"
+                            comportamento=""
+                            textoComportamento=""
+                            pontoForte=""
+                            textoPontoForte=""
+                            pontosMelhoria=""
+                            textoPontosMelhoria=""
+                            textoMotivacoes=""
+                            textoValores=""
                         />
                     </Grid>
                     <Grid item xs={6} sm={3}>
@@ -43,6 +113,14 @@ export default function Results() {
                             title="Águia"
                             image={Aguia}
                             imageTitle="Águia"
+                            comportamento=""
+                            textoComportamento=""
+                            pontoForte=""
+                            textoPontoForte=""
+                            pontosMelhoria=""
+                            textoPontosMelhoria=""
+                            textoMotivacoes=""
+                            textoValores=""
                         />
                     </Grid>
                     <Grid item xs={6} sm={3}>
@@ -50,6 +128,14 @@ export default function Results() {
                             title="Gato"
                             image={Gato}
                             imageTitle="gato"
+                            comportamento=""
+                            textoComportamento=""
+                            pontoForte=""
+                            textoPontoForte=""
+                            pontosMelhoria=""
+                            textoPontosMelhoria=""
+                            textoMotivacoes=""
+                            textoValores=""
                         />
                     </Grid>
                 </Grid>
